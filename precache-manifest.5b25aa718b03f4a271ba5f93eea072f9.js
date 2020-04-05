eb14979a0fc258bca24a6f65b66534113b208209
shyam sundar <shyamsundar055@gmail.com>
2020-04-05 23:33:24 +0530

    Updates

---
+++ precache-manifest.5b25aa718b03f4a271ba5f93eea072f9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79dd970251e3a84c2a54628218f3c7c5",
+    "url": "/Stats-App/index.html"
+  },
+  {
+    "revision": "0612981d850796179b68",
+    "url": "/Stats-App/static/css/main.a25a2208.chunk.css"
+  },
+  {
+    "revision": "9b6ed6eee449dc1196aa",
+    "url": "/Stats-App/static/js/2.6f5402b1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Stats-App/static/js/2.6f5402b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0612981d850796179b68",
+    "url": "/Stats-App/static/js/main.ac3e31e8.chunk.js"
+  },
+  {
+    "revision": "05a22202e429123df19e",
+    "url": "/Stats-App/static/js/runtime-main.351f480d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Stats-App/static/media/logo.5d5d9eef.svg"
+  }
+]);
