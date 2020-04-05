d449b953b9ca5745badb0d1b5476336aa4d69c5f
shyam sundar <shyamsundar055@gmail.com>
2020-04-06 02:25:59 +0530

    Final update for version 1.0

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import IndiaFlag from './images/indiaflag.png'
+import coronavirus from './images/coronavirus.png'
 import './App.css';
 import axios from 'axios';
 import Graph from './graph';
@@ -68,7 +68,7 @@ function App() {
     });
   }
 
-  const changeSelectedCountry = (event)=> {
+  const changeSelectedCountry = (event) => {
     setSelectedCountry(event.target.value);
     //console.log(event.target.value);
   }
@@ -80,50 +80,64 @@ function App() {
           <div className="container d-flex justify-content-center">
             <div className="row">
               <div className="col-sm-12 text-center">
-                <h3 className="display-4">COVID-19 Stats</h3>
+                <h3 className="display-4">
+                  <img src={coronavirus} width="60" height="60" />&nbsp;
+                  <span className="align-bottom">Coronavirus Statistics</span>
+                </h3>
                 <br />
-                <select className="form-control" value={selectedCountry} onChange={changeSelectedCountry}>
-                  {
-                    countryList.map((country, key) =>
-                      <option value={country} key={key} >{country}</option>
-                    )
-                  }
-                </select>
               </div>
             </div>
           </div>
           {Object.keys(statsData).length > 0 ?
-            <> 
-              <br />
-              <h1 className="display-3"> 
-                  <b>{statsData.country}</b>
-              </h1>
-              <p>
-                <small>Last updated : {new Date(statsData.time).toLocaleString()}</small>
-              </p>
-              <br />
-              <h2 className="display-4">Total Cases<br /><span style={{ color: "rgb(48, 247, 238)" }}>{statsData.cases.total}</span></h2>
-              <br />
-              <h2 className="display-4">Recovered <br /><span style={{ color: "#8ACA2B" }}>{statsData.cases.recovered}</span></h2>
-              <br />
-              <h2 className="display-4">Deaths <br /><span style={{ color: "rgb(255, 38, 27)" }}>{statsData.deaths.total}</span></h2>
-              <br />
-              <br />
-              <div className="container">
-                <div className="row">
-                  <div className="col-sm">
-                    <p className="h3">Active Cases<br /><span style={{ color: "#ffd230" }}>{statsData.cases.active}</span></p>
+            <div className="container-fluid">
+              <div className="row">
+                <div className="col-md-12 text-center">
+                  <div className="d-flex justify-content-center">
+                    <select className="form-control" value={selectedCountry} onChange={changeSelectedCountry} style={{ width: "250px" }}>
+                      {
+                        countryList.map((country, key) =>
+                          <option value={country} key={key} >{country}</option>
+                        )
+                      }
+                    </select>
                   </div>
-                  <div className="col-sm">
-                    <p className="h3">New Cases<br /><span style={{ color: "#ffd230" }}>{statsData.cases.new ? statsData.cases.new : "-"}</span></p>
-                  </div>
-                  <div className="col-sm">
-                    <p className="h3">New Deaths<br /><span style={{ color: "#ffd230" }}>{statsData.deaths.new ? statsData.deaths.new : "-"}</span></p>
+                  <br />
+                  <h1 className="display-3">
+                    <b>{statsData.country}</b>
+                  </h1>
+                  <p>
+                    <small>Last updated : {new Date(statsData.time).toLocaleString()}</small>
+                  </p>
+                  <br />
+                  <h2 className="display-4">Total Cases<br /><span style={{ color: "rgb(48, 247, 238)" }}>{statsData.cases.total}</span></h2>
+                  <br />
+                  <h2 className="display-4">Recovered <br /><span style={{ color: "#8ACA2B" }}>{statsData.cases.recovered}</span></h2>
+                  <br />
+                  <h2 className="display-4">Deaths <br /><span style={{ color: "rgb(255, 38, 27)" }}>{statsData.deaths.total}</span></h2>
+                  <br />
+                  <br />
+                  <div className="container">
+                    <div className="row">
+                      <div className="col-sm">
+                        <p className="h3">Active Cases<br /><span style={{ color: "#ffd230" }}>{statsData.cases.active}</span></p>
+                      </div>
+                      <div className="col-sm">
+                        <p className="h3">New Cases<br /><span style={{ color: "#ffd230" }}>{statsData.cases.new ? statsData.cases.new : "-"}</span></p>
+                      </div>
+                      <div className="col-sm">
+                        <p className="h3">New Deaths<br /><span style={{ color: "#ffd230" }}>{statsData.deaths.new ? statsData.deaths.new : "-"}</span></p>
+                      </div>
+                    </div>
+
                   </div>
                 </div>
+                {/* <div className="col-md-8 text-center">
+                  
+                </div> */}
                 {/* <Graph statsHistoryData={statsHistoryData}/> */}
               </div>
-            </>
+            </div>
+
             : ""
           }
         </> :
